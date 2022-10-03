e453da173e4fec103228b94ba3a9d9681a7e1ff5
romantrety <114093642+22057221@users.noreply.github.com>
2022-10-03 18:55:53 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,143 @@
+﻿//#include <iostream>
+//using namespace std;
+//int main() {
+	//case 1
+	//int day;
+	//cin >> day;
+	//switch (day) {
+	//case 1: cout << "monday" << endl; break;
+	//case 2: cout << "tueday" << endl; break;
+	//case 3: cout << "wednesday" << endl; break;
+	//case 4: cout << "thuesday" << endl; break;
+	//case 5: cout << "friday" << endl; break;
+	//case 6: cout << "saturday" << endl; break;
+	//case 7: cout << "sunday" << endl; break;
+	//default: cout << "wrong day number" << endl;
+//	}
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+	//Case2.Дано целое число K.Вывести строку - описание оценки, соответствующей числу K(1 — «плохо», 2 — «неудовлетворительно», 3 — «удовлетворительно», 4 — «хорошо», 5 — «отлично»).Если K не лежит в диапазоне 1–5,
+	//int mark;
+	//cin >> mark;
+	//switch (mark) {
+	//case 1: cout << "плохо" << endl; break;
+	//case 2: cout << "удатварительно" << endl; break;
+	//case 3: cout << "хорошо" << endl; break;
+	//case 4: cout << "отлично" << endl; break;
+	//case 5: cout << "замечательно" << endl; break;
+	//}
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+	//Case4◦
+		//.Дан номер месяца — целое число в диапазоне 1–12(1 — январь, 2 —
+			//февраль и т.д.).Определить количество дней в этом месяце для невисокосного года. 
+	//setlocale(LC_ALL, "Russian");
+	//int k;
+	//cin >> k;
+	//switch (k) {
+	//case 1: cout << "январь 31" << endl; break;
+	//case 2: cout << "февраль 28" << endl; break;
+	//case 3: cout << "март 31" << endl; break;
+	//case 4: cout << "апрель 30" << endl; break;
+	//case 5: cout << "май 31" << endl; break;
+	//case 6: cout << "июнь 30" << endl; break;
+	//case 7: cout << "июль 31" << endl; break;
+	//case 8: cout << "августа 31" << endl; break;
+	//case 9: cout << "сентябрь 30" << endl; break;
+	//case 10: cout << "октябрь 31" << endl; break;
+	//case 11: cout << "ноябрь 30" << endl; break;
+	//case 12: cout << "декабрь 31" << endl; break;
+	//default: cout << "неправильный номер месяца" << endl;
+	//}
+//}
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	//Case5.Арифметические действия над числами пронумерованы следующим
+		//образом : 1 — сложение, 2 — вычитание, 3 — умножение, 4 — деление.Дан
+		//номер действия N(целое число в диапазоне 1–4) и вещественные числа A
+		//и B(В не равно 0).Выполнить над числами указанное действие и вывести
+		//результат.
+	/*int n;
+	cin >> n;
+	switch (n) {
+	case 1: cout << "Сложение" << endl; break;
+	case 2: cout << "вычитание" << endl; break;
+	case 3: cout << "умножение" << endl; break;
+	case 4: cout << "деление" << endl; break;
+	default: cout << "неправильная цифра" << endl;
+	}
+}*/
+
+
+
+
+//If1. Дано целое число. Если оно является положительным, то прибавить к
+//нему 1; в противном случае не изменять его.Вывести полученное число.
+/*int a;
+cin >> a;
+if (a > 0);
+cout << a;
+}
+*/
+
+
+
+//If2.Дано целое число.Если оно является положительным, то прибавить к
+	//нему 1; в противном случае вычесть из него 2. Вывести полученное число.
+	//int a;
+	//cin >> a;
+	//if (a > 0)
+	//else
+		//a = a - 2;
+	//cout << a;
+//}
+
+
+
+	//If3. Дано целое число. Если оно является положительным, то прибавить к
+	//нему 1; если отрицательным, то вычесть из него 2; если нулевым, то
+		//заменить его на 10. Вывести полученное число
+	//int x;
+	//cin >> x;
+	//if (x > 0) {
+		//cout << x;
+	//}
+	//else {
+		//if (x < 0) {
+			//x = x - 2;
+			//cout << x;
+		//}
+		//else {
+			//x = 10;
+			//cout << x;
+		//}
+	//}
+//}
+
+
+
+
+
+
+
+
+
